275a5c79dbbadb6365d868b62fb2b88b953c2afa
snoodledoodledong <pizzakaleb2010@gmail.com>
2026-02-11 15:55:58 -0800

    did intake stuff

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -4,11 +4,37 @@
 
 package frc.robot.subsystems.Intake;
 
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
-  public IntakeSubsystem() {}
+
+  private final SparkMax rollerMotor;
+  private final SparkMax armMotor;
+
+
+  private static final class IntakeConstants{
+    public static final int ROLLERMOTORID = 0;
+    public static final int ARMMOTORID = 0;
+
+    public static final double INTAKESPEED = 0.75;
+    public static final double ARMSPEED = 0.5;
+    public static final int CURRENTLIMIT = 30;
+  }
+
+  public IntakeSubsystem() {
+    rollerMotor = new SparkMax(IntakeConstants.ROLLERMOTORID, MotorType.kBrushless);
+    rollerMotor.setInverted(false);
+    rollerMotor.setIdleMode(SparkMax.IdleMode.kCoast);
+
+
+    armMotor = new SparkMax(IntakeConstants.ARMMOTORID, MotorType.kBrushless);
+
+  }
 
   @Override
   public void periodic() {
