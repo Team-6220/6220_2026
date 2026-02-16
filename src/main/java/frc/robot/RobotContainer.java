6507ace20773108094f4271a90f357dc5f70589a
leetokuda <leetokuda@yahoo.com>
2026-02-16 08:56:58 -0800

    test error checking

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+>>>>>>>>>>>ERRROR<<<<<<<<<<<<<<<<
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.wpilibj.GenericHID;
