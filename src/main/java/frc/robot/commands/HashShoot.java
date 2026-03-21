520aff8698c1648db54d2eece14938db4aeb39f1
tokutofu <n8thant@gmail.com>
2026-03-21 10:15:56 -0700

    undo some stuff

--- src/main/java/frc/robot/commands/HashShoot.java
+++ src/main/java/frc/robot/commands/HashShoot.java
@@ -43,7 +43,7 @@ public class HashShoot extends Command {
   @Override
   public void execute() {
     m_angler.setAngle(degrees);
-    m_shoot.runAtTargetVelocity(rpm, false);
+    m_shoot.runAtTargetVelocity(rpm);
   }
 
   // Called once the command ends or is interrupted.
