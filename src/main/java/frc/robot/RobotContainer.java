0d627079230fbb95b4b54881b73e91d50cb9e5d8
tokutofu <n8thant@gmail.com>
2026-04-01 20:23:11 -0700

    GOOD STUFFFFFFF

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -15,14 +16,14 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.AlignAndMove;
-import frc.robot.commands.Autos.BasicAutoBlue;
-import frc.robot.commands.Autos.BasicAutoRed;
+import frc.robot.commands.ArmToPositionCommand;
 import frc.robot.commands.HashShoot;
 import frc.robot.commands.ManualArm;
 import frc.robot.commands.PassToAlliance;
-import frc.robot.commands.ShooterTESTER;
 import frc.robot.commands.SwerveCom;
 import frc.robot.commands.TestRollerCommand;
+import frc.robot.commands.Autos.BasicAutoBlue;
+import frc.robot.commands.Autos.BasicAutoRed;
 import frc.robot.subsystems.Drive.Swerve;
 import frc.robot.subsystems.Intake.ArmSubsystem;
 import frc.robot.subsystems.Intake.BeltSubsystem;
@@ -68,32 +69,6 @@ public class RobotContainer {
     s_Swerve.setDefaultCommand(
         new SwerveCom(s_Swerve, m_driverController, m_driverController.leftBumper()));
 
-    // Prespin flywheels when limelight sees a tag
-    // m_shooter.setDefaultCommand(
-    //     Commands.run(
-    //         () -> {
-    //           if (m_shooter.getDist() > 0) {
-    //             m_shooter.setTopGroupVelocityRPS(m_shooter.getTopTargetRPM() / 60.0);
-    //           } else {
-    //             m_shooter.stop();
-    //           }
-    //         },
-    //         m_shooter));
-
-    // m_angler.setDefaultCommand(
-    //     Commands.run(
-    //         () -> {
-    //           double input = -m_driverController.getLeftY(); // negative so up = up
-    //           if (Math.abs(input) < ANGLER_DEADBAND) {
-    //             m_angler.stop();
-    //           } else {
-    //             m_angler.setSpeed(input * ANGLER_MAX_SPEED);
-    //           }
-    //         },
-    //         m_angler));
-
-    // belt.setDefaultCommand(new TestBeltCommand());
-
     arm.setDefaultCommand(new ManualArm(m_joystick));
 
     autoChooser = AutoBuilder.buildAutoChooser();
@@ -118,28 +93,27 @@ public class RobotContainer {
    * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
-  private void configureBindings() {
+private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    Trigger angleUp = new Trigger(() -> m_buttonBoard.getRawButton(5));
-    Trigger angleDown = new Trigger(() -> m_buttonBoard.getRawButton(6));
+    Trigger angleUp = new Trigger(() -> m_buttonBoard.getRawButton(15));
+    Trigger angleDown = new Trigger(() -> m_buttonBoard.getRawButton(16));
     Trigger intakeOut = new Trigger(() -> m_buttonBoard.getRawButton(2));
     Trigger intakeIn = new Trigger(() -> m_buttonBoard.getRawButton(1));
     Trigger pass = m_driverController.rightBumper();
-    Trigger resetEncoder = new Trigger(() -> m_buttonBoard.getRawButton(3));
+    Trigger resetEncoder = new Trigger(() -> m_buttonBoard.getRawButton(13));
     Trigger manualArm = new Trigger(() -> m_joystick.getRawButton(1));
+    Trigger anglerTest0 = new Trigger(() -> m_buttonBoard.getRawButton(3));
+    Trigger arm0 = new Trigger(() -> m_buttonBoard.getRawButton(5));
+    Trigger arm90 = new Trigger(() -> m_buttonBoard.getRawButton(6));
+    Trigger armReset = new Trigger(() -> m_buttonBoard.getRawButton(14));
+
     resetEncoder.onTrue(Commands.runOnce(() -> m_angler.resetEncoder()));
-
-    // Test buttons for angler PID: button 4 -> set to 1.0,
-    Trigger anglerTestOne = new Trigger(() -> m_buttonBoard.getRawButton(4));
-    Trigger anglerTest0 = new Trigger(() -> m_buttonBoard.getRawButton(11));
-
-    anglerTestOne.onTrue(
-        Commands.run(() -> m_angler.setAngle(m_angler.getAnglerAngle()), m_angler)
-            .until(() -> m_angler.isAtAngle(m_angler.getAnglerAngle())));
+    armReset.onTrue(Commands.runOnce(() -> arm.resetEncoder()));
 
     anglerTest0.onTrue(
         Commands.run(() -> m_angler.setAngle(0), m_angler).until(() -> m_angler.isAtAngle(0)));
 
+        
     m_driverController
         .y()
         .onTrue(new InstantCommand(() -> s_Swerve.zeroHeading(m_driverController.getHID())));
@@ -155,10 +129,7 @@ public class RobotContainer {
         .onTrue(
             Commands.run(() -> m_angler.setAngle(0), m_angler).until(() -> m_angler.isAtAngle(0)));
 
-    m_driverController
-        .b().whileTrue(new TestRollerCommand(false));
-
-    m_driverController.x().whileTrue(new ShooterTESTER(m_shooter, belt));
+    // m_driverController.x().whileTrue(new ShooterTESTER(m_shooter, belt));
 
     pass.whileTrue(new PassToAlliance(m_angler, m_shooter, belt));
 
@@ -172,32 +143,21 @@ public class RobotContainer {
         .leftTrigger()
         .whileTrue(
             new AlignAndMove(s_Swerve, m_driverController, m_driverController.rightBumper()));
+    
+    arm0.onTrue(new ArmToPositionCommand(arm, -2));
 
-    // ========== SysId Buttons (button board) ==========
-    // Button 8: Quasistatic Forward (slow ramp up)
-    // Button 9: Quasistatic Reverse (slow ramp down)
-    // Button 10: Dynamic Forward (step voltage)
-    // Button 11 is already used for anglerTest0, so using button 12 instead
-    // Button 12: Dynamic Reverse (step voltage reverse)
-    // new Trigger(() -> m_joystick.getRawButton(3))
-    //     .whileTrue(m_shooter.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    // new Trigger(() -> m_joystick.getRawButton(4))
-    //     .whileTrue(m_shooter.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    // new Trigger(() -> m_joystick.getRawButton(5))
-    //     .whileTrue(m_shooter.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    // new Trigger(() -> m_joystick.getRawButton(6))
-    //     .whileTrue(m_shooter.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-  }
+    arm90.onTrue(new ArmToPositionCommand(arm, -29.785476684570312));
+    }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
+public Command getAutonomousCommand() {
     System.out.println("auto: " + autoChooser.getSelected());
     return autoChooser.getSelected();
-  }
+}
   // An example command will be run in autonomous
 
 }
