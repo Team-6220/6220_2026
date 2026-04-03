bfa40a5ef36efeb978741b509a137c3b959936fa
tokutofu <n8thant@gmail.com>
2026-04-03 12:07:28 -0700

    autoalign in auto

--- src/main/java/frc/robot/commands/Autos/BasicAutoRed.java
+++ src/main/java/frc/robot/commands/Autos/BasicAutoRed.java
@@ -5,10 +5,13 @@
 package frc.robot.commands.Autos;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.AlignAndMove;
 import frc.robot.commands.HashShoot;
 import frc.robot.subsystems.Drive.Swerve;
 import frc.robot.subsystems.Intake.BeltSubsystem;
@@ -24,16 +27,18 @@ public class BasicAutoRed extends SequentialCommandGroup {
   private ShooterSubsystem m_shooter;
   private BeltSubsystem m_belt;
   private Swerve s_swerve;
+  private CommandXboxController controller;
 
   /** Creates a new StrightAuto. */
   public BasicAutoRed(
-      Swerve s_swerve, AnglerSubsystem m_angler, ShooterSubsystem m_shooter, BeltSubsystem m_belt) {
+      Swerve s_swerve, AnglerSubsystem m_angler, ShooterSubsystem m_shooter, BeltSubsystem m_belt, CommandXboxController controller) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     this.s_swerve = s_swerve;
     this.m_angler = m_angler;
     this.m_shooter = m_shooter;
     this.m_belt = m_belt;
+    this.controller = controller;
     addRequirements(s_swerve, m_angler, m_belt, m_shooter);
     addCommands(
         // AutoBuilder.pathfindToPose(new Pose2d(-1, 0, new Rotation2d()),
@@ -45,6 +50,7 @@ public class BasicAutoRed extends SequentialCommandGroup {
             .withTimeout(1.2),
         new RunCommand(() -> s_swerve.drive(new Translation2d(0, 0), 0, true, false))
             .withTimeout(0.2),
+        new AlignAndMove(s_swerve, controller, controller.rightBumper()).withTimeout(2),
         new HashShoot(m_angler, m_shooter, m_belt).withTimeout(4),
         new PrintCommand("done"));
   }
