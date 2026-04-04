4dd9c00be5a4d472888343f83c5e53fbad218839
tokutofu <n8thant@gmail.com>
2026-04-04 08:02:34 -0700

    auto align blue auto

--- src/main/java/frc/robot/commands/Autos/BasicAutoBlue.java
+++ src/main/java/frc/robot/commands/Autos/BasicAutoBlue.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.AlignAndMove;
 import frc.robot.commands.HashShoot;
 import frc.robot.subsystems.Drive.Swerve;
 import frc.robot.subsystems.Intake.BeltSubsystem;
@@ -24,16 +26,18 @@ public class BasicAutoBlue extends SequentialCommandGroup {
   private ShooterSubsystem m_shooter;
   private BeltSubsystem m_belt;
   private Swerve s_swerve;
+  private CommandXboxController controller;
 
   /** Creates a new StrightAuto. */
   public BasicAutoBlue(
-      Swerve s_swerve, AnglerSubsystem m_angler, ShooterSubsystem m_shooter, BeltSubsystem m_belt) {
+      Swerve s_swerve, AnglerSubsystem m_angler, ShooterSubsystem m_shooter, BeltSubsystem m_belt, CommandXboxController controller) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     this.s_swerve = s_swerve;
     this.m_angler = m_angler;
     this.m_shooter = m_shooter;
     this.m_belt = m_belt;
+    this.controller = controller;
     addRequirements(s_swerve, m_angler, m_belt, m_shooter);
     addCommands(
         // AutoBuilder.pathfindToPose(new Pose2d(-1, 0, new Rotation2d()),
@@ -45,6 +49,7 @@ public class BasicAutoBlue extends SequentialCommandGroup {
             .withTimeout(1.2),
         new RunCommand(() -> s_swerve.drive(new Translation2d(0, 0), 0, true, false))
             .withTimeout(0.2),
+        new AlignAndMove(s_swerve, controller, controller.rightBumper()).withTimeout(2),
         new HashShoot(m_angler, m_shooter, m_belt).withTimeout(4),
         new PrintCommand("done"));
   }
